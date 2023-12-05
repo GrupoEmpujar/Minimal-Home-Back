6db47e2cf681b28bf9109c7d5ea2c120a914b219
Lucas Colque <lucascolque35@gmail.com>
2023-12-05 09:45:55 -0300

    archivo .json

--- src/connection/mongoDB.js
+++ src/connection/mongoDB.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const connectDB = async({HOST, PORT, NAME})=>{
+const connectDB = async ({ HOST, PORT, NAME }) => {
     try {
         console.log(HOST, PORT, NAME);
         const uri = `mongodb://${HOST}:${PORT}/${NAME}`
@@ -11,5 +11,12 @@ const connectDB = async({HOST, PORT, NAME})=>{
 }
 mongoose.connection.on("open", () => {
     console.log("Base de datos conectada");
+});
+mongoose.connection.on('error', function (err) {
+    console.log('Error de conexión a la base de datos: ' + err);
+});
+
+mongoose.connection.on('disconnected', function () {
+    console.log('Conexión a la base de datos cerrada');
 });
 module.exports = connectDB;
